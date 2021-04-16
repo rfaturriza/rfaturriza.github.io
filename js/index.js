7b0fdb67cefbeeae745cdea28d2d822d1acfbe53
Rizky Faturriza <56538058+rfaturriza@users.noreply.github.com>
2021-04-17 00:06:36 +0700

    update index.js

--- js/index.js
+++ js/index.js
@@ -19,7 +19,7 @@ $(document).ready(function(){
           hour = minute * 60,
           day = hour * 24;
   
-    let birthday = "May 19, 2021 02:38:00",
+    let birthday = "May 19, 2021 00:00:00",
         countDown = new Date(birthday).getTime(),
         x = setInterval(function() {    
   
