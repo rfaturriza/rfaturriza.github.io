10afd6a2b9d3222b7e05b8ea945d853d34592f4e
Rizky Faturriza <rfaturriza@gmail.com>
2023-10-28 20:34:38 +0700

    change check out my

--- src/components/sections/hero.js
+++ src/components/sections/hero.js
@@ -77,10 +77,10 @@ const Hero = () => {
   const five = (
     <a
       className="email-link"
-      href="https://www.newline.co/courses/build-a-spotify-connected-app"
+      href="https://www.linkedin.com/in/rfaturriza"
       target="_blank"
       rel="noreferrer">
-      Check out my course!
+      Check out my LinkedIn
     </a>
   );
 
